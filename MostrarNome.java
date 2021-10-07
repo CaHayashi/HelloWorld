3246f5f500abf38cbe5770ea155074fa785f90bd
CaHayashi <61543498+CaHayashi@users.noreply.github.com>
2021-10-07 10:23:03 -0300

    Update MostrarNome.java
    Commit alterando de Steve Jobs para Salvio Padlipskas.

--- MostrarNome.java
+++ MostrarNome.java
@@ -1,8 +1,8 @@
 public class MostrarNome{
 
 	public static void main(String[] args) {
-	String nome = "Steve Jobs";	
-	System.out.printf("Nome: %s",nome);
+	String nome = "Salvio Padlipskas";	
+	System.out.printf("Nome: ",nome);
 
 	}
 }
